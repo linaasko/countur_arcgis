68d1e315c268a38c584a8031c3ae46292df52858
Lina <kokoshalina@gmail.com>
2024-04-28 00:49:51 +0300

    parameters are created

---
+++ pythonProject/contur.py
@@ -0,0 +1,19 @@
+import arcpy
+inraster = arcpy.GetParameterAsText(0)
+if inraster == '#' or not inraster:
+    inraster = r"E:\university\3 course\programming in gis\lections\arcpy, pr5\Lesson1\Lesson1\foxlake"
+
+outcontur = arcpy.GetParameterAsText(1)
+if outcontur == '#' or not outcontur:
+    outcontur = r"E:\university\3 course\programming in gis\lections\arcpy, pr5\Lesson1\Lesson1\countur.shp"
+import arcpy
+inraster = arcpy.GetParameterAsText(0)
+if inraster == '#' or not inraster:
+    inraster = r"E:\university\3 course\programming in gis\lections\arcpy, pr5\Lesson1\Lesson1\foxlake"
+
+outcontur = arcpy.GetParameterAsText(1)
+if outcontur == '#' or not outcontur:
+    outcontur = r"E:\university\3 course\programming in gis\lections\arcpy, pr5\Lesson1\Lesson1\countur.shp"
+contour_interval = arcpy.GetParameterAsText(2)
+if contour_interval == '#' or not contour_interval:
+    contour_interval = '25'
